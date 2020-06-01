8f0025af03b4a209f08b87128bab5906e5be4a15
Eric Li <messier.object.111@gmail.com>
2020-05-31 23:14:50 -0400

    Create MoveZeroes.java

---
+++ algorithms/src/com/alg/pointers/MoveZeroes.java
@@ -0,0 +1,33 @@
+package com.alg.pointers;
+
+public class MoveZeroes {
+    /**
+     * @param nums: an integer array
+     * @return: nothing
+     */
+    public void moveZeroes(int[] nums) {
+        // write your code here
+        for(int i = 0; i <= nums.length - 1; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == 0 && nums[j] != 0) {
+                    swap(nums, i, j);
+                }
+            }
+        }
+    }
+
+    private void swap(int[]nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes ins = new MoveZeroes();
+        int[] nums = {0, 1, 0, 3, 12};
+        ins.moveZeroes(nums);
+        for(int num : nums) {
+            System.out.println(num);
+        }
+    }
+}
