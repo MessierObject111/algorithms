f0643298e5f0b3aa410800570ce26e05c60981e1
Eric Li <messier.object.111@gmail.com>
2021-06-14 22:31:34 -0400

    Update CallableTest.java
    Big question mark on how my code give such result...

--- algorithms/src/com/multiThread/future/CallableTest.java
+++ algorithms/src/com/multiThread/future/CallableTest.java
@@ -14,6 +14,7 @@ public class CallableTest {
                 for(int i = 0; i < 1000_000; i++) {
                     counter.addAndGet(1);
                 }
+                System.out.println("[" + Thread.currentThread() + "] finished callable 1");
                 return counter.get();
             }
         };
@@ -22,23 +23,32 @@ public class CallableTest {
             for(int i = 0; i < 1000_000; i++) {
                 counter.addAndGet(1);
             }
+            System.out.println("[" + Thread.currentThread() + "] finished callable 2");
             return (Long) counter.get();
         };
+        System.out.println("=====================Test 1===========================");
 
-        Future<Long> future1 = threadPool.submit(callable1);
-        Future future2 = threadPool.submit(callable2);
+        Future<Long> future1 = threadPool.submit(callable1);// Will be executed by another thread
+        Future<Long> future2 = threadPool.submit(callable2);// Will be executed by another thread
 
         // NOTE: Future will take the value at the moment when the tasks finishes, and
         // even that value gets updated later, it won't care. Thus in result, it may show
         // numbers other than 2000,000 for future1/future2, because one task may have been
         // executing
 
+        // Both callables are sharing single counter with an AtomicLong
+
+        //TODO: Why I am getting weird results that one of future1 / future2 will get number that is less than 2000,000?
+
         try {
-            while(!future1.isDone() || !future2.isDone()) {
+            // Will be executed by main thread
+            while(!(future1.isDone() && future2.isDone())) {
                 Thread.sleep(100);
             }
-            System.out.println(future1.get());
-            System.out.println(future2.get());
+
+            System.out.println("[" + Thread.currentThread() + "] future1: " + future1.get());
+            System.out.println("[" + Thread.currentThread() + "] future2: " + future2.get());
+
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -46,7 +56,7 @@ public class CallableTest {
             e.printStackTrace();
         }
 
-
+        System.out.println("=====================Test 2===========================");
         try {
             // If call() directly, main thread won't split sub threads, and will execute in
             // main thread instead.
@@ -58,6 +68,8 @@ public class CallableTest {
             e.printStackTrace();
         }
 
+
+        System.out.println("=====================Test 3===========================");
         FutureTask<Long> futureTask = new FutureTask<>(callable1);
         Thread t = new Thread(futureTask);
         t.start();
