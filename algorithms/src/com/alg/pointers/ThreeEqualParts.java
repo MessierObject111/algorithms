a5794d362da0406a5b0f327a9a9bc52610eb4688
Eric Li <messier.object.111@gmail.com>
2021-07-17 23:41:10 -0400

    Update ThreeEqualParts.java

--- algorithms/src/com/alg/pointers/ThreeEqualParts.java
+++ algorithms/src/com/alg/pointers/ThreeEqualParts.java
@@ -20,19 +20,22 @@ package com.alg.pointers;
 public class ThreeEqualParts {
     public int[] threeEqualParts(int[] arr) {
         int result[] = new int[2];
-        for(int i = 0; i < arr.length - 2; i++) {
-            for(int j = i + 1; j < arr.length - 1; j++) {
-                int p1 = 0;
-                int p2 = i+1;
-                int p3 = j;
-                p1 = findNextNoneZeroIndex(p1, i+1, arr);
-                p2 = findNextNoneZeroIndex(p2, j, arr);
+        for(int i = 0; i <= arr.length - 2; i++) {
+            for(int j = i + 2; j <= arr.length - 1; j++) {
+                int p1 = 0; //->i
+                int p2 = i+1; // ->j-1
+                int p3 = j; // -> arr.length
+//                if(i == 1 && j == 4) {
+//                    System.out.println("Bingo");
+//                }
+                p1 = findNextNoneZeroIndex(p1, i, arr);
+                p2 = findNextNoneZeroIndex(p2, j-1, arr);
                 p3 = findNextNoneZeroIndex(p3, arr.length - 1, arr);
-                for(; p1 <= i && p2 < j && p3 < arr.length - 1; p1++, p2++, p3++) {
+                for(; p1 <= i && p2 < j && p3 <= arr.length - 1; p1++, p2++, p3++) {
                     if(arr[p1] != arr[p2] || arr[p2] != arr[p3]) {
                         break;
                     }
-                    if(p1 == i+1 && p2 == j && p3 == arr.length - 1) {
+                    if(p1 == i && p2 == j-1 && p3 == arr.length - 1) {
                         result[0] = i;
                         result[1] = j;
                         return result;
@@ -47,7 +50,7 @@ public class ThreeEqualParts {
 
     private int findNextNoneZeroIndex (int p, int boundary, int[] arr) {
         if(p == boundary) return p;
-        while (arr[p] == 0 && p <= boundary) {
+        while (p < boundary && arr[p] == 0) {
             p++;
         }
         return p;
@@ -55,8 +58,16 @@ public class ThreeEqualParts {
 
     public static void main(String[] args) {
         ThreeEqualParts sol = new ThreeEqualParts();
-        int[] arr = {1,0,1,0,1}; // {1, 3}
+        int[] arr = {1,0,1,0,1}; // {0, 3}; from 0 - i(0), then i+1(1) - j-1(2), then j(3) - 4
         int[] result = sol.threeEqualParts(arr);
         System.out.println(result[0] + ", " + result[1]);
+
+        int[] arr2 = {0,1,0,1,1}; // {1, 4};
+        int[] result2 = sol.threeEqualParts(arr2);
+        System.out.println(result2[0] + ", " + result2[1]);
+
+//        int[] arr3 = {0,0,0,0,0};//[0,4];
+//        int[] result3 = sol.threeEqualParts(arr3);
+//        System.out.println(result3[0] + ", " + result3[1]);
     }
 }
