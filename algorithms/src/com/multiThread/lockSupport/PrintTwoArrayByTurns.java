0a8bf01473474b4038c5d6205d45446a96a57567
ludwiglevo <ludwiglevo@gmail.com>
2021-07-28 22:24:08 -0400

    Create PrintTwoArrayByTurns.java

---
+++ algorithms/src/com/multiThread/lockSupport/PrintTwoArrayByTurns.java
@@ -0,0 +1,38 @@
+package com.multiThread.lockSupport;
+
+import java.util.concurrent.locks.LockSupport;
+
+public class PrintTwoArrayByTurns {
+    static Thread t1 = null, t2 = null;
+
+    public static void main(String[] args) {
+        char[] arr1 = "12345".toCharArray();
+        char[] arr2 = "abcde".toCharArray();
+
+
+        t1 = new Thread (()->{
+
+            for(char c : arr1) {
+                System.out.println(c);
+                LockSupport.unpark(t2);
+                LockSupport.park();
+            }
+            
+        }, "t1");
+
+        t2 = new Thread (()->{
+
+            for(char c : arr2) {
+                LockSupport.park();
+                System.out.println(c);
+                LockSupport.unpark(t1);
+            }
+
+        }, "t2");
+
+
+        t1.start();
+        t2.start();
+    }
+
+}
