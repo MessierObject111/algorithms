554456124c288e3257b10aa66df163d77eaeea58
Eric Li <messier.object.111@gmail.com>
2022-02-06 22:47:22 -0500

    Create SinglyLinkedListNode.java

---
+++ algorithms/src/com/alg/common/SinglyLinkedListNode.java
@@ -0,0 +1,17 @@
+package com.alg.common;
+
+public class SinglyLinkedListNode {
+    public SinglyLinkedListNode next;
+    public int data;
+    public SinglyLinkedListNode (int data) {
+        this.data = data;
+    }
+
+//    public static SinglyLinkedListNode build(int[] data) {
+//        for (int i=0; i<data.length; i++) {
+//            SinglyLinkedListNode node = new SinglyLinkedListNode(data[i]);
+//
+//        }
+//    }
+}
+
