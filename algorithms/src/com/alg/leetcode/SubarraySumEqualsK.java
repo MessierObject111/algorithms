b935e5b1439c32ad5bd66fc26be12f9493e3e224
ludwiglevo <ludwiglevo@gmail.com>
2022-02-15 23:04:06 -0500

    Update SubarraySumEqualsK.java
    Initial commit that uses brute force

--- algorithms/src/com/alg/leetcode/SubarraySumEqualsK.java
+++ algorithms/src/com/alg/leetcode/SubarraySumEqualsK.java
@@ -23,10 +23,42 @@ package com.alg.leetcode;
  * -107 <= k <= 107
  */
 public class SubarraySumEqualsK {
-    public int subarraySum(int[] nums, int k) {
-
+    /**
+     * Use brute force to iterate each possible combo and their sum to
+     * find out the # of total sub arrays.
+     * Time Limit Exceeded when submitted in leetcode.
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySumBruteForce(int[] nums, int k) {
+        int total = 0;
+        for(int i = 0; i<= nums.length-1; i++) {
+            for(int j = i; j<= nums.length-1; j++) {
+                int sum = 0;
+                for(int p = i; p <= j; p++) {
+                    sum += nums[p];
+                }
+                if(sum == k) {
+                    total++;
+                }
+            }
+        }
+        return total;
     }
-    public static void main (String[] args) {
 
+    public static void main (String[] args) {
+        int[] nums_0 = {1, 2, 3, 4};
+        int[] nums_1 = {-1, 2, -3, 4};
+        int[] nums_2 = {23, 22, 13, 47};
+        int[] nums_3 = {117, 43, -83, 342, 232, 94, 35, 23, 364, 339};
+        SubarraySumEqualsK sol = new SubarraySumEqualsK();
+        System.out.println(sol.subarraySumBruteForce(nums_0, 3));//2
+        System.out.println(sol.subarraySumBruteForce(nums_0, 7));//1
+        System.out.println(sol.subarraySumBruteForce(nums_0, 8));//0
+
+        System.out.println(sol.subarraySumBruteForce(nums_1, 1));//2
+        System.out.println(sol.subarraySumBruteForce(nums_1, 2));//2 (2; -1+2-3+4)
+        System.out.println(sol.subarraySumBruteForce(nums_1, -2));//1
     }
 }
