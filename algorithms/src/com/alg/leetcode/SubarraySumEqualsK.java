e00a2c37cae0eb706029ece7e16f918fc984a86f
ludwiglevo <ludwiglevo@gmail.com>
2022-02-16 00:15:54 -0500

    Update SubarraySumEqualsK.java

--- algorithms/src/com/alg/leetcode/SubarraySumEqualsK.java
+++ algorithms/src/com/alg/leetcode/SubarraySumEqualsK.java
@@ -47,18 +47,41 @@ public class SubarraySumEqualsK {
         return total;
     }
 
+    public int subarraySumCumulative(int[] nums, int k) {
+        int total = 0;
+        int[] cumulativeMap = new int[nums.length + 1];
+        for(int i = 1; i <= nums.length; i++)
+            cumulativeMap[i] = cumulativeMap[i - 1] + nums[i - 1];
+        for(int start = 0; start< nums.length; start++) {
+            for(int end = start+1; end <= nums.length; end++) {
+                if(cumulativeMap[end] - cumulativeMap[start] == k) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+
     public static void main (String[] args) {
-        int[] nums_0 = {1, 2, 3, 4};
+        int[] nums_0 = {1, 2, 3, 4}; // 0, 1, 3, 6, 10
         int[] nums_1 = {-1, 2, -3, 4};
         int[] nums_2 = {23, 22, 13, 47};
         int[] nums_3 = {117, 43, -83, 342, 232, 94, 35, 23, 364, 339};
         SubarraySumEqualsK sol = new SubarraySumEqualsK();
-        System.out.println(sol.subarraySumBruteForce(nums_0, 3));//2
-        System.out.println(sol.subarraySumBruteForce(nums_0, 7));//1
-        System.out.println(sol.subarraySumBruteForce(nums_0, 8));//0
+//        System.out.println(sol.subarraySumBruteForce(nums_0, 3));//2
+//        System.out.println(sol.subarraySumBruteForce(nums_0, 7));//1
+//        System.out.println(sol.subarraySumBruteForce(nums_0, 8));//0
+//
+//        System.out.println(sol.subarraySumBruteForce(nums_1, 1));//2
+//        System.out.println(sol.subarraySumBruteForce(nums_1, 2));//2 (2; -1+2-3+4)
+//        System.out.println(sol.subarraySumBruteForce(nums_1, -2));//1
 
-        System.out.println(sol.subarraySumBruteForce(nums_1, 1));//2
-        System.out.println(sol.subarraySumBruteForce(nums_1, 2));//2 (2; -1+2-3+4)
-        System.out.println(sol.subarraySumBruteForce(nums_1, -2));//1
+        System.out.println(sol.subarraySumCumulative(nums_0, 3));//2
+        System.out.println(sol.subarraySumCumulative(nums_0, 7));//1
+        System.out.println(sol.subarraySumCumulative(nums_0, 8));//0
+
+        System.out.println(sol.subarraySumCumulative(nums_1, 1));//2
+        System.out.println(sol.subarraySumCumulative(nums_1, 2));//2 (2; -1+2-3+4)
+        System.out.println(sol.subarraySumCumulative(nums_1, -2));//1
     }
 }
