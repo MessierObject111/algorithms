f2895987504d95ec4f15a596b73a04cce4acef82
Eric Li <messier.object.111@gmail.com>
2022-02-27 13:18:07 -0500

    Update FirstBadVersion20220227.java

--- algorithms/src/com/alg/binarySearch/FirstBadVersion20220227.java
+++ algorithms/src/com/alg/binarySearch/FirstBadVersion20220227.java
@@ -4,25 +4,33 @@ public class FirstBadVersion20220227 {
     //Apparently I forgot how this was done all together
     public boolean[] commits;
     public int firstBadVersion(int n) {
+        long start = System.currentTimeMillis();
         if(isBadVersion(1)) return 1;
-        return binarySearch(1, n);
+        int result = binarySearch(1, n);
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println("Time used: "+ timeElapsed);//Time lapsed here proved recursion doesn't take noticeable more time than iteration
+        return result;
     }
 
     private int binarySearch(int start, int end) {
-        if(start + 1 >= end) return isBadVersion(start) ? start : end;
+        if(start + 1 >= end) { // BUG ALERT!!! INT overflow, should be (end-start <= 1)
+            return isBadVersion(start) ? start : end;
+        }
         int mid = start + (end - start)/2;//To avoid int overflow when doing (start + end)/2
         if(isBadVersion(mid)) return binarySearch(start, mid);
         return binarySearch(mid + 1, end);
     }
 
     private boolean isBadVersion(int index) {
+//        System.out.println(index + " checked!");
         return this.commits[index];
     }
 
     public static void main(String[] args) {
         FirstBadVersion20220227 sol = new FirstBadVersion20220227();
-        int n = 1000;
-        int firstBadVersion = 998;
+        int n = 2147483645;
+        int firstBadVersion = 2147483644;
 
         sol.commits = new boolean[n];
 
