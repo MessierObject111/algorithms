1e65e4f93fd2f92453444903e3700afda92c77c7
ludwiglevo <ludwiglevo@gmail.com>
2022-05-10 14:28:23 -0400

    Create FlattenMultiLevelDoublyLinkedList.java

---
+++ algorithms/src/com/alg/tree/FlattenMultiLevelDoublyLinkedList.java
@@ -0,0 +1,13 @@
+package com.alg.tree;
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+public class FlattenMultiLevelDoublyLinkedList {
+    public Node flatten(Node head) {
+        //TODO: Damn this question is hard to me
+        return head;
+    }
+}
