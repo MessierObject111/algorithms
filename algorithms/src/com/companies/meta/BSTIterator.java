af97a38ed347032d931f3e7e0d8230800806d46a
Eric Li <messier.object.111@gmail.com>
2022-06-29 00:20:40 -0400

    Create BSTIterator.java

---
+++ algorithms/src/com/companies/meta/BSTIterator.java
@@ -0,0 +1,75 @@
+package com.companies.meta;
+
+import com.alg.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+ * 173. Binary Search Tree Iterator
+ * Medium
+ *
+ * 6242
+ *
+ * 402
+ *
+ * Add to List
+ *
+ * Share
+ * Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+ *
+ * BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+ * boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+ * int next() Moves the pointer to the right, then returns the number at the pointer.
+ * Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+ *
+ * You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+ */
+class BSTIterator {
+    private Queue<Integer> q = new LinkedList<>();
+
+
+    private void inOrderTraversal(TreeNode node) {
+        if(node != null) {
+            inOrderTraversal(node.left);
+            q.offer(node.val);
+            inOrderTraversal(node.right);
+        }
+        return;
+    }
+
+    public BSTIterator(TreeNode root) {
+        inOrderTraversal(root);
+    }
+
+    public int next() {
+        return q.poll();
+    }
+
+    public boolean hasNext() {
+        return !q.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
