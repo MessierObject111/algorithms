61b444e1b9223ba80ab48e0fc0e794c078d0c572
Eric Li <messier.object.111@gmail.com>
2022-07-24 11:34:30 -0400

    Update ContinuousSubArraySum.java

--- algorithms/src/com/companies/meta/ContinuousSubArraySum.java
+++ algorithms/src/com/companies/meta/ContinuousSubArraySum.java
@@ -1,12 +1,25 @@
 package com.companies.meta;
 
+import com.java.se.inheritancePolymorphism.question9.C;
+
 public class ContinuousSubArraySum {
-    //Brute force: time limit exceeded
+    //O(N^2): time limit exceeded
+    //What else can you do to a algo that needs to check every sub array combo? The minimum is O(N^2)! Yes it still
+    // exceeded time limit...
     public boolean checkSubarraySum(int[] nums, int k) {
         if(nums.length < 2) return false;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
         for(int i = 0; i < nums.length - 1; i++) {
             for(int j = i + 1; j < nums.length; j++) {
-                int sum = sum(nums, i, j);
+                int sum = 0;
+                if(i == 0) {
+                    sum = dp[j-1] + nums[j];
+                    dp[j] = sum;
+                    //System.out.println("dp[" + j + "]: "+ sum);
+                } else {
+                    sum = dp[j] - dp[i] + nums[i];
+                }
                 if (sum % k == 0) return true;
             }
         }
@@ -20,5 +33,11 @@ public class ContinuousSubArraySum {
             i++;
         }
         return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2 , 12};
+        ContinuousSubArraySum sol = new ContinuousSubArraySum();
+        System.out.println(sol.checkSubarraySum(nums, 6));
     }
 }
