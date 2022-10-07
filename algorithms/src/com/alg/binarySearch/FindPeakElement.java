7e04086374b3d501b9ccf53dced87b487477a1fb
Eric Li <messier.object.111@gmail.com>
2022-10-06 23:46:10 -0400

    Update FindPeakElement.java

--- algorithms/src/com/alg/binarySearch/FindPeakElement.java
+++ algorithms/src/com/alg/binarySearch/FindPeakElement.java
@@ -91,6 +91,73 @@ public class FindPeakElement {
         }
     }
 
+    //2022-10-06 I totally forgot about details of this question lol
+
+//    A peak element is an element that is strictly greater than its neighbors.
+//
+//    Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+//
+//    You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+//
+//    You must write an algorithm that runs in O(log n) time.
+//
+//
+//
+//    Example 1:
+//
+//    Input: nums = [1,2,3,1]
+//    Output: 2
+//    Explanation: 3 is a peak element and your function should return the index number 2.
+//    Example 2:
+//
+//    Input: nums = [1,2,1,3,5,6,4]
+//    Output: 5
+//    Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+//
+//
+//    Constraints:
+//
+//            1 <= nums.length <= 1000
+//            -231 <= nums[i] <= 231 - 1
+//    nums[i] != nums[i + 1] for all valid i.
+//
+    //This question is not clear on what happens when the array size is less than 3, which doesn't qualify the definition of peak-
+    //It will lack the two neigbhours needed to compare with.
+    //[3,2,1]: the peak is index 0, value 3
+    public int findPeakElementC(int[] nums) {
+        if(nums.length ==1) return 0;
+        if(nums.length ==2) return nums[0] > nums[1] ? 0 : 1;
+
+        //1.Linear search:
+
+        // for(int i = 0; i < nums.length; i++) {
+        //     if(isPeak(nums, i)) return i;
+        // }
+
+        //2. Binary search; log(N)
+        return search(nums, 0, nums.length - 1);
+
+        // return 0;
+    }
+
+    private boolean isPeak(int[] nums, int i) {
+        if(i == 0) return nums[0] > nums[1] ? true : false;
+        if(i == nums.length-1) return nums[nums.length-2] < nums[nums.length-1] ? true : false;
+        if(nums[i - 1] < nums[i] && nums[i] > nums[i+1]) return true;
+        return false;
+    }
+
+    private int search(int[] nums, int i, int j) {
+        if(i == j) {
+            return i;
+        }
+        int m = i + (j - i)/2;
+        if(nums[m] < nums[m+1]){
+            return search(nums, m + 1, j);
+        }
+        return search(nums, i, m);
+    }
+
 
 
     public static void main(String[] args) {
