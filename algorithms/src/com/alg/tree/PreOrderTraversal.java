104a63b2979df72806493d977e69a73218420996
Eric Li <messier.object.111@gmail.com>
2023-07-17 16:37:59 -0400

    Create PreOrderTraversal.java

---
+++ algorithms/src/com/alg/tree/PreOrderTraversal.java
@@ -0,0 +1,23 @@
+package com.alg.tree;
+import com.alg.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+public class PreOrderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root == null) return list;
+        helper(root, list);
+        return list;
+    }
+
+    private void helper(TreeNode node, List<Integer> list){
+        if(node!=null) {
+            list.add(node.val);
+            helper(node.left, list);
+            helper(node.right, list);
+        }
+    }
+}
